860a3f4c96a9aa07bad5be799238353981980406
Raluca Parvan <parvanraluca0@gmail.com>
2023-09-27 15:45:35 +0200

    Fix project images bug

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import Label from "../components/Label"
+import image from "../assets/passwordGenerator.png"
 
 export default function Project(props){
 
@@ -7,8 +8,9 @@ export default function Project(props){
     return (
       <div className="project">
         <div className="project-visuals">
-          {props.desktopImage && <img src={props.desktopImage} alt="desktop image" className="project-img" />}
-          {props.phoneImage && <img src={props.phoneImage} alt="phone image" className="phone-ver" />}
+          {props.desktopImage && <img src={`src/assets/${props.desktopImage}`} alt="desktop image" className="project-img" />}
+          {props.phoneImage && <img src={`src/assets/${props.phoneImage}`} alt="phone image" className="phone-ver" />}
+          {console.log(props.desktopImage)}
         </div> 
         <h3 className="project-title">{props.title}</h3>
         <div className="labels">
