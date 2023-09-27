f1843a1f635c1bd5c02b2bd3fe48bfbade34174a
Raluca Parvan <parvanraluca0@gmail.com>
2023-09-27 16:00:55 +0200

    Add links for projects images

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types'
 import Label from "../components/Label"
-import image from "../assets/passwordGenerator.png"
 
 export default function Project(props){
 
@@ -8,9 +7,8 @@ export default function Project(props){
     return (
       <div className="project">
         <div className="project-visuals">
-          {props.desktopImage && <img src={`src/assets/${props.desktopImage}`} alt="desktop image" className="project-img" />}
-          {props.phoneImage && <img src={`src/assets/${props.phoneImage}`} alt="phone image" className="phone-ver" />}
-          {console.log(props.desktopImage)}
+          {props.desktopImage && <img src={props.desktopImage} alt="desktop image" className="project-img" />}
+          {props.phoneImage && <img src={props.phoneImage} alt="phone image" className="phone-ver" />}
         </div> 
         <h3 className="project-title">{props.title}</h3>
         <div className="labels">
