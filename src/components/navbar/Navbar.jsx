db01e8fd0047252e6ca415db92549a2ff0d49c62
Raluca Pârvan <parvanraluca0@gmail.com>
2024-02-05 19:49:23 +0100

    Clear up the UI

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
         <nav className="navbar">
             <a className="nav-element" href='#home'>Home</a>
             <a className="nav-element" href='#projects'>Projects</a>
-            <a className="nav-element" href='#'>About</a>
-            <a className="nav-element" href='#'>Contact</a>
+            <a className="nav-element" href='#about'>About</a>
+            <a className="nav-element" href='#contact'>Contact</a>
         </nav>
     )
 }
